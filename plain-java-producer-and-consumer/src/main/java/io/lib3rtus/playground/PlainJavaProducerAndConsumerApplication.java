3b8cdd002e7c4540f5b69186f81da2f8d07dd709
Djordje Velickovic <lib3rtus@gmail.com>
2022-05-30 18:55:29 +0200

    Plain java project shell

---
+++ plain-java-producer-and-consumer/src/main/java/io/lib3rtus/playground/PlainJavaProducerAndConsumerApplication.java
@@ -0,0 +1,13 @@
+package io.lib3rtus.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlainJavaProducerAndConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlainJavaProducerAndConsumerApplication.class, args);
+    }
+
+}
---
+++ plain-java-producer-and-consumer/src/test/java/io/lib3rtus/playground/PlainJavaProducerAndConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lib3rtus.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlainJavaProducerAndConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
